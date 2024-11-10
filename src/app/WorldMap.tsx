ed3560fbb5f7fe0d54c8e469542bd29cb5cb611f
xiongamy <xiongamy@users.noreply.github.com>
2024-11-10 15:00:37 -0800

    functional zoom buttons

--- src/app/WorldMap.tsx
+++ src/app/WorldMap.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, {useState} from "react";
 import { ComposableMap, Geographies, ZoomableGroup, Marker, Geography, Graticule, Sphere } from "react-simple-maps";
 import GLASODGeography from "./geographies/glasod"
 import CountriesGeography from "./geographies/countries";
@@ -8,12 +8,35 @@ import ZoomButtons from "./ZoomButtons";
 
 const geoUrl = "/countries.topojson.json";
 
+interface Position {
+  coordinates: [number, number];
+  zoom: number;
+}
+
+const ZOOM_FACTOR = 1.5;
+
 const WorldMap: React.FC = () => {
+  const [zoom, setZoom] = useState(1);
+  const [coordinates, setCoordinates] = useState<[number, number]>([0.0, 0.0]);
+
+  function onZoomIn() {
+    setZoom(zoom * ZOOM_FACTOR);
+  }
+
+  function onZoomOut() {
+    setZoom(zoom / ZOOM_FACTOR);
+  }
+
+  function onMoveEnd(position: Position) {
+    setCoordinates(position.coordinates);
+    setZoom(position.zoom);
+  }
+
   return (
     <div>
-      <ZoomButtons />
-      <ComposableMap projectionConfig={{ scale: 100 }} height={window.innerHeight * .7}>
-        <ZoomableGroup>
+      <ZoomButtons onZoomIn={onZoomIn} onZoomOut={onZoomOut} />
+      <ComposableMap projectionConfig={{ scale: 120 }} height={window.innerHeight * .7}>
+        <ZoomableGroup zoom={zoom} center={coordinates} onMoveEnd={onMoveEnd}>
           <Graticule stroke="#DDD" clipPath="url(#rsm-sphere)" suppressHydrationWarning />
           <Sphere stroke="#FF5533" strokeWidth={2} id="rsm-sphere" fill="transparent" />
           <CountriesGeography />
