04b81f2b0358c29a982f1678bef0349391d0a5fa
Charles Lien <charleslien97@gmail.com>
2024-11-10 11:04:03 -0800

    Add multiple uploads

--- src/app/CsvUploader.tsx
+++ src/app/CsvUploader.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import Papa from 'papaparse'; // Import the papaparse library
 
+import Point, { haversineDistance } from 'nearestNeighbor';
+
 // Define the type for the CSV data you are expecting
 interface CsvData {
   [key: string]: string;
@@ -9,7 +11,18 @@ interface CsvData {
 
 const CsvUploader: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
-  const [csvData, setCsvData] = useState<CsvData[]>([]); // Store parsed CSV data
+  const [allCsvData, setAllCsvData] = useState<{string: CsvData}>({}); // Store parsed CSV data
+
+  const addCsvData = (csvName: string, newCsvData: CsvData) => {
+    setAllCsvData((prevCsvData) => ({ ...prevCsvData, [csvName]: newCsvData }));
+  };
+
+  const removeCsvData = (csvName: string) => {
+    setAllCsvData((prevCsvData) => {
+      const { [csvName]: _, ...rest } = prevCsvData;
+      return rest;
+    });
+  };
 
   // Handle file selection
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,7 +38,7 @@ const CsvUploader: React.FC = () => {
     Papa.parse(file, {
       complete: (result) => {
         console.log('Parsed CSV result:', result); // Log the parsed CSV data
-        setCsvData(result.data as CsvData[]); // Save parsed data to state
+        addCsvData(file.name, result.data); // Save parsed data to state
       },
       header: true, // Treat the first row as headers
       skipEmptyLines: true, // Skip empty lines
@@ -49,15 +62,25 @@ const CsvUploader: React.FC = () => {
       <table>
         <thead>
           <tr>
-            {csvData.length > 0 && Object.keys(csvData[0]).map((key) => <th key={key}>{key}</th>)}
+            {file && allCsvData[file.name] && allCsvData[file.name].length > 0 && Object.keys(allCsvData[file.name][0]).map((key) => <th key={key}>{key}</th>)}
           </tr>
         </thead>
         <tbody>
-          {csvData.map((row, index) => (
+          {file && allCsvData[file.name] && allCsvData[file.name].map((row, index) => (
             <tr key={index}>
               {Object.values(row).map((value, i) => (
                 <td key={i}>{value}</td>
               ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h3>Csv Files</h3>
+      <table>
+        <tbody>
+          {Object.entries(allCsvData).map(([key, _]) => (
+            <tr key={key}>
+              <td>{key}</td>
             </tr>
           ))}
         </tbody>
