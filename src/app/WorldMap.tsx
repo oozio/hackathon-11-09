8525f7885e733e5d4f2811fcf97677c1eb6ef5a4
oozio <xxiaohanw@gmail.com>
2024-11-10 13:45:04 -0800

    refactor

--- src/app/WorldMap.tsx
+++ src/app/WorldMap.tsx
@@ -1,17 +1,10 @@
 "use client";
 import React from "react";
 import { ComposableMap, Geographies, ZoomableGroup, Marker, Geography, Graticule,Sphere  } from "react-simple-maps";
+import GLASODGeography from "./geographies/glasod"
+import CountriesGeography from "./geographies/countries";
 
-const geoUrl = "/GLASOD.topojson.json";
-
-const strokeWidth = 0.1;
-const sevColors = {
-  0: "green",
-  1: "yellow",
-  2: "orange",
-  3: "red",
-  4: "maroon"
-}
+const geoUrl = "/countries.topojson.json";
 
 const WorldMap: React.FC = () => {
   return (
@@ -28,27 +21,8 @@ const WorldMap: React.FC = () => {
       >
         <Graticule stroke="#DDD" clipPath="url(#rsm-sphere)" suppressHydrationWarning/>
         <Sphere stroke="#FF5533" strokeWidth={2} id="rsm-sphere" fill="transparent"/>
-        <Geographies 
-          geography={geoUrl} 
-          
-          parseGeographies={(geos) => {
-            return geos.map((g) => {
-              // Process geographies here...
-              console.log(g)
-              return g
-            })
-          }}
-      >
-          {({ geographies }) =>
-            geographies.map((geo) => (
-              <Geography key={geo.rsmKey} geography={geo} fill="transparent" stroke="#000" strokeWidth={0.1}   style={{
-                default: { fill: sevColors[geo.properties.SEVERITY] },
-                hover: { fill: "blue"  },
-                pressed: { fill: "#02A" },
-              }}/>
-            ))
-          }
-        </Geographies>
+        <CountriesGeography />
+        <GLASODGeography />
       </ZoomableGroup>
     </ComposableMap>
   );
---
+++ src/app/geographies/glasod.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import { ComposableMap, Geographies, ZoomableGroup, Marker, Geography, Graticule,Sphere  } from "react-simple-maps";
+
+const geoUrl = "/GLASOD.topojson.json";
+
+const strokeWidth = 0.1;
+const sevColors = {
+  0: "green",
+  1: "yellow",
+  2: "orange",
+  3: "red",
+  4: "maroon"
+}
+
+const GLASODGeography: React.FC = () => {
+  return (
+        <Geographies 
+          geography={geoUrl} 
+          
+          parseGeographies={(geos) => {
+            return geos.map((g) => {
+              // Process geographies here...
+              console.log(g)
+              return g
+            })
+          }}
+      >
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography key={geo.rsmKey} geography={geo} fill="transparent" stroke="#000" strokeWidth={0.1}   style={{
+                default: { fill: sevColors[geo.properties.SEVERITY] },
+                hover: { fill: "blue"  },
+                pressed: { fill: "#02A" },
+              }}/>
+            ))
+          }
+        </Geographies>
+  );
+};
+
+export default GLASODGeography;
