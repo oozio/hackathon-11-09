120aa9f1e5f920992fc7b03c8bb9df7514cfdee1
Charles Lien <charleslien97@gmail.com>
2024-11-09 23:57:29 -0800

    Change to use gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // basePath: "/hackathon-11-09",
-  output: "export",
-  reactStrictMode: true,
+  distDir: 'build',
 };
 
 export default nextConfig;
